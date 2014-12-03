90d0ae9eeb27aee6e9919966408ce3f3a4c11269
Luca <Luca@Luca-PC>
2014-12-03 20:08:58 +0100

    First Commit by Luca

--- src/te20110225/Executor.java
+++ src/te20110225/Executor.java
@@ -1,7 +1,6 @@
 package te20110225;
 import java.util.TreeMap;
 
-
 public class Executor {
 
 	private TreeMap<Long, Runnable> jobs;
@@ -9,26 +8,22 @@ public class Executor {
 	public Executor(){
 		jobs = new TreeMap<>();
 	}
-	
 	public void start(){
 		new Consumer().start();
 	}
-
 	public synchronized void execute(Runnable job, long delay){
 		jobs.put(System.currentTimeMillis()+delay, job);
 		notifyAll();
 	}
 
 	private class Consumer extends Thread {
-
 		public void run() {
 			while(true){
 				synchronized(Executor.this){
 					while(jobs.isEmpty()) {
 						try {
 							Executor.this.wait();
-						} catch (InterruptedException e) {
-						}					
+						} catch (InterruptedException e) {}					
 					}
 					if(jobs.firstKey()<=System.currentTimeMillis()){
 						System.out.println("Sono il consumer, eseguo qualcosa.");
@@ -37,14 +32,10 @@ public class Executor {
 					} else {
 						try {
 							Executor.this.wait(jobs.firstKey()-System.currentTimeMillis());
-						} catch (InterruptedException e) {
-						}
+						} catch (InterruptedException e) {}
 					}
 				}
-
 			}
 		}
 	}
-
-
 }
--- src/te20110225/Main.java
+++ src/te20110225/Main.java
@@ -1,8 +1,8 @@
 package te20110225;
 
-public class Main {
-
-	public static void main(String[] args) {
+public class Main 
+{
+	public static void main(String[] args){
 		Executor ex = new Executor();
 		ex.start();
 		ex.execute(new Job(1), 10000);
@@ -10,19 +10,17 @@ public class Main {
 		ex.execute(new Job(3), 3000);		
 		ex.execute(new Job(4), 6000);		
 	}
-
-	private static class Job implements Runnable {
-
+	
+	private static class Job implements Runnable 
+	{
 		private int id;
 		
 		public Job(int id){
 			this.id = id;
 		}
-		
 		@Override
 		public void run() {
 			System.out.println("Sono il job: "+id);
-		}
-		
+		}		
 	}
 }
